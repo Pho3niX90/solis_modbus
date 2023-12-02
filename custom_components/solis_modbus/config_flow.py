ea590dbd846a58b37469c297fd403245833c8722
Shaun J.V.Nieuwenhuizen <shaunjvn@gmail.com>
2023-12-02 21:53:38 +0200

    feat: add service to write back to address

--- custom_components/solis_modbus/config_flow.py
+++ custom_components/solis_modbus/config_flow.py
@@ -31,7 +31,7 @@ class ModbusConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         modbus_controller = ModbusController(user_input["host"], user_input.get("port", 502))
         try:
             modbus_controller.connect()
-            modbus_controller.read_register(33093)
+            modbus_controller.read_input_register(33093)
             return True
         except ConnectionError:
             return False
--- custom_components/solis_modbus/const.py
+++ custom_components/solis_modbus/const.py
@@ -1,4 +1,4 @@
 DOMAIN = "solis_modbus"
 CONTROLLER = "modbus_controller"
-VERSION = "1.0.1"
+VERSION = "1.0.2"
 POLL_INTERVAL_SECONDS = 5
