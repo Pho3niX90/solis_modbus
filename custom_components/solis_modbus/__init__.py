ec47a2b3ac9103814a465236dddc497fe9608117
Shaun J.V.Nieuwenhuizen <shaunjvn@gmail.com>
2025-01-30 11:33:21 +0200

    fixes: #79

--- custom_components/solis_modbus/__init__.py
+++ custom_components/solis_modbus/__init__.py
@@ -11,7 +11,7 @@ from .modbus_controller import ModbusController
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORMS = [Platform.SENSOR, Platform.NUMBER, Platform.SWITCH, Platform.TIME]
+PLATFORMS = [Platform.NUMBER, Platform.SWITCH, Platform.TIME]
 
 SCHEME_HOLDING_REGISTER = vol.Schema(
     {
@@ -67,10 +67,8 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     _LOGGER.debug(f'config entry host = {host}, post = {port}')
 
     # Set up the platforms associated with this integration
-    for component in PLATFORMS:
-        hass.async_create_task(hass.config_entries.async_forward_entry_setup(entry, component))
-        _LOGGER.debug(f"async_setup_entry: loading: {component}")
-        await asyncio.sleep(1)
+    await hass.config_entries.async_forward_entry_setups(entry, [Platform.SENSOR])
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     await asyncio.sleep(20)
     return True
 
