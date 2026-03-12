639ab7021b221c4a0f3bbe971e7ff0e3d4b3f10a
Shaun JVN <shaunjvn@gmail.com>
2026-03-12 08:05:36 +0200

    https://github.com/Pho3niX90/solis_modbus/issues/338

--- custom_components/solis_modbus/data/solis_config.py
+++ custom_components/solis_modbus/data/solis_config.py
@@ -50,7 +50,7 @@ SOLIS_INVERTERS = [
                    features=[InverterFeature.SMART_PORT]),
     InverterConfig(model="S6-EH2P", wattage=[9600, 11400, 12000, 14000, 16000], phases=3, type=InverterType.HYBRID,
                    features=[InverterFeature.SMART_PORT]),
-    InverterConfig(model="S6-EH3P", wattage=[8000, 10000, 12000, 15000], phases=3, type=InverterType.HYBRID,
+    InverterConfig(model="S6-EH3P", wattage=[8000, 10000, 12000, 15000, 29900, 30000, 40000, 49000, 50000, 60000], phases=3, type=InverterType.HYBRID,
                    features=[InverterFeature.SMART_PORT]),
     InverterConfig(model="S6-EO1P", wattage=[4000, 5000], phases=1, type=InverterType.HYBRID,
                    features=[InverterFeature.SMART_PORT]),
